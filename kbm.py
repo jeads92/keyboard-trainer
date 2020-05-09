e275f31fbc223a630f58e41d7b3279969298be11
jeads92 <eadsjosh@msu.edu>
2020-05-09 12:09:22 -0400

    Used classes to reduce redundancy. Variable and character sets stored as class variables so they do not have to be created more than once.

--- kbm.py
+++ kbm.py
@@ -2,183 +2,62 @@ import random
 import os
 from nltk.corpus import words
 
-def mainscreen():
-    print('What do you want to train?')
-    print('1.)Numbers\n2.)Letters\n3.)Symbols\n4.)Keypad\n5.) For words.\n')
-    game_type = input('Enter game choice ')
-    if game_type == '1':
-        numbers_trainer()
-    elif game_type == '2':
-        letter_trainer()
-    elif game_type == '3':
-        symbol_trainer()
-    elif game_type == '4':
-        keypad_trainer()
-    elif game_type == '5':
-        word_trainer()
-
-def numbers_trainer():
-    '''Creates a tuple that houses numbers, assigns a variable that is a number
-    then the user must correctly guess that number.'''
-    numbers_var = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
-    combo = 0
-    user_selection = ''
-    answer_status = 'Ready up.'
-    while user_selection != 'quit':
-        os.system('cls')
-        number_selection = numbers_var[random.randint(0,9)]
-        print(f'Current Streak: {combo}!\n')
-        print(answer_status)
-        print(f'Number is: {number_selection} ')
-        user_selection = input('Type the number: ')
-        if user_selection == 'quit':
-            print('Game Ended')
-        elif user_selection == number_selection:
-            combo += 1
-            answer_status = 'Correct!'
-        else:
-            combo = 0
-            answer_status = 'Wrong'
-            
-            
-def keypad_trainer():
-    '''uses a tuple to contain the numbers and symbols on the keypad and 
-    randomly generates which character the user must input.'''
-    keypad_var = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '/', '*', '-', '+', '.')
-    combo = 0
-    user_selection = ''
-    answer_status = 'Ready up.'
-    while user_selection != 'quit':
-        os.system('cls')
-        keypad_selection = keypad_var[random.randint(0, 14)]
-        print(f'Current Streak: {combo}!\n')
-        print(answer_status)
-        print(f'Character is: {keypad_selection} ')
-        user_selection = input('Type the character: ')
-        if user_selection == 'quit':
-            print('Game Ended')
-        elif user_selection == keypad_selection:
-            combo += 1
-            answer_status = 'Correct!'
-        else:
-            combo = 0
-            answer_status = 'Wrong'
-            
-            
-def letter_trainer():
-    '''uses a tuple to store all letters. Then user used must type in the 
-    letter.'''
-    letter_var = ('a', 'b', 'c', 'd', 'e', 'f','g', 'h', 'i', 'j', 'k', 'l',\
+class CharacterRunner():
+    def __init__(self):
+        self.numbers_var = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+        self.keypad_var = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '/', '*', '-', '+', '.')
+        self.letter_var = ('a', 'b', 'c', 'd', 'e', 'f','g', 'h', 'i', 'j', 'k', 'l',\
                   'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
-    combo = 0
-    user_selection = ''
-    answer_status = 'Ready up.'
-    while user_selection != 'quit':
-        os.system('cls')
-        letter_selection = letter_var[random.randint(0, 25)]
-        print(f'Current Streak: {combo}!\n')
-        print(answer_status)
-        print(f'Character is: {letter_selection} ')
-        user_selection = input('Type the character: ')
-        if user_selection == 'quit':
-            print('Game Ended')
-        elif user_selection == letter_selection:
-            combo += 1
-            answer_status = 'Correct!'
-        else:
-            combo = 0
-            answer_status = 'Wrong'
-    
-    
-def symbol_trainer():
-    '''uses a tuple to store all symbols. Then user used must type in the 
-    symbol.'''
-    symbol_var = ('!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_',\
+        self.symbol_var = ('!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_',\
                   '+', '=', '[', '{', ']', '}', ':', ';', "'", '|', '\\', ',',\
                       '<', '.', '>', '?', '/', '`', '~')
-    combo = 0
-    user_selection = ''
-    answer_status = 'Ready up.'
-    while user_selection != 'quit':
-        os.system('cls')
-        symbol_selection = symbol_var[random.randint(0, int(len(symbol_var) - 1))]
-        print(f'Current Streak: {combo}!\n')
-        print(answer_status)
-        print(f'Character is: {symbol_selection} ')
-        user_selection = input('Type the character: ')
-        if user_selection == 'quit':
-            print('Game Ended')
-        elif user_selection == symbol_selection:
-            combo += 1
-            answer_status = 'Correct!'
-        else:
-            combo = 0
-            answer_status = 'Wrong'
+        self.word_list = words.words()
+        self.combo = 0
+        self.user_selection = ''
+        self.answer_status = 'Ready Up'
         
-def word_trainer():
-    '''uses a list to store all a big list of words. Then user used must type in the 
-    words.'''
-    word_list = words.words()
-    combo = 0
-    user_selection = ''
-    answer_status = 'Ready up.'
-    while user_selection != 'q':
-        os.system('cls')
-        word_selection = word_list[random.randint(0, 236735)]
-        print(f'Current Streak: {combo}!\n')
-        print(answer_status)
-        print(f'Word is: {word_selection} ')
-        user_selection = input('Type the word: ')
-        if user_selection == 'q':
-            print('Game Ended')
-        elif user_selection == word_selection:
-            combo += 1
-            answer_status = 'Correct!'
-        else:
-            combo = 0
-            answer_status = 'Wrong'
+        
+    def run_game(self, game_choice):
+        '''
+        This used the predefined character sets to genereate the character the
+        user must type in correctly. Combo is updated if correct or incorrect.
+        '''
+        while self.user_selection != 'ENDGAME':
+            os.system('cls')
+            character = game_choice[random.randint(0,len(game_choice) - 1)]
+            print(f'Current Streak: {self.combo}!\n')
+            print(self.answer_status)
+            print(f'Number is: {character} ')
+            self.user_selection = input('Type the number: ')
+            if self.user_selection == 'ENDGAME':
+                print('Game Ended')
+            elif self.user_selection == character:
+                self.combo += 1
+                self.answer_status = 'Correct!'
+            else:
+                self.combo = 0
+                self.answer_status = 'Wrong'
+        
+    def mainscreen(self):
+        '''
+        This lets the user choose which type of character set they want to
+        practice.
+        '''
+        print('What do you want to train?')
+        print('1.)Numbers\n2.)Letters\n3.)Symbols\n4.)Keypad\n5.) For words.\n')
+        game_type = input('Enter game choice ')
+        if game_type == '1':
+            self.run_game(self.numbers_var)
+        elif game_type == '2':
+            self.run_game(self.letter_var)
+        elif game_type == '3':
+            self.run_game(self.symbol_var)
+        elif game_type == '4':
+            self.run_game(self.keypad_var)
+        elif game_type == '5':
+            self.run_game(self.word_list)
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+        
+def start_game():
+    start = CharacterRunner()
+    start.mainscreen()
--- keyboard-trainer.py
+++ keyboard-trainer.py
@@ -1,5 +1,4 @@
 # follow pep-8. https://www.python.org/dev/peps/pep-0008/
 
 import kbm
-#test for git. test. another test#
-kbm.mainscreen()
+kbm.start_game()
