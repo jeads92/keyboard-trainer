c2d6c07f24fa5611c90f9bd7865dd666ad0a3d0d
jeads92 <eadsjosh@msu.edu>
2020-05-12 09:55:37 -0400

    Formatted code to comply with PEP8 standards.

--- kbm.py
+++ kbm.py
@@ -3,31 +3,32 @@ import os
 from nltk.corpus import words
 import time
 
+
 class CharacterRunner():
     def __init__(self):
         self.numbers_var = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
-        self.keypad_var = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '/', '*', '-', '+', '.')
-        self.letter_var = ('a', 'b', 'c', 'd', 'e', 'f','g', 'h', 'i', 'j', 'k', 'l',\
-                  'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
-        self.symbol_var = ('!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_',\
-                  '+', '=', '[', '{', ']', '}', ':', ';', "'", '|', '\\', ',',\
-                      '<', '.', '>', '?', '/', '`', '~')
+        self.keypad_var = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                           '/', '*', '-', '+', '.')
+        self.letter_var = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+                           'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                           'u', 'v', 'w', 'x', 'y', 'z')
+        self.symbol_var = ('!', '@', '#', '$', '%', '^', '&', '*', '(', ')',
+                           '-', '_', '+', '=', '[', '{', ']', '}', ':', ';',
+                           "'", '|', '\\', ',', '<', '.', '>', '?', '/', '`',
+                           '~')
         self.word_list = words.words()
-        self.all_var = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '/',\
-                        '*', '-', '+', '.', 'a', 'b', 'c', 'd', 'e', 'f','g',\
-                            'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',\
-                                'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'\
-                                    '!', '@', '#', '$', '%', '^', '&',\
-                                        '(', ')', '_', '=', '[',\
-                                            '{', ']', '}', ':', ';', "'",\
-                                                '|', '\\', ',', '<', '.',\
-                                                    '>', '?', '`', '~') 
+        self.all_var = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '/',
+                        '*', '-', '+', '.', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+                        'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                        's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '!', '@', '#',
+                        '$', '%', '^', '&', '(', ')', '_', '=', '[', '{', ']',
+                        '}', ':', ';', "'", '|', '\\', ',', '<', '.', '>',
+                        '?', '`', '~')
         self.combo = 0
         self.user_selection = ''
         self.answer_status = 'Ready Up'
         self.rando_string = ''
-        
-        
+
     def run_game(self, game_choice):
         '''
         This uses the predefined character sets to genereate the character the
@@ -36,7 +37,7 @@ class CharacterRunner():
         char_time = ''
         while self.user_selection != 'ENDGAME':
             os.system('cls')
-            character = game_choice[random.randint(0,len(game_choice) - 1)]
+            character = game_choice[random.randint(0, len(game_choice) - 1)]
             print(f'Current Streak: {self.combo}!\n')
             if type(char_time) != str:
                 print(f'Input time: {char_time:0.4f}')
@@ -55,7 +56,6 @@ class CharacterRunner():
                 self.combo = 0
                 self.answer_status = 'Wrong'
 
-
     def build_a_string(self):
         '''
         This builds a randomly-generated string from all characters on the
@@ -67,14 +67,16 @@ class CharacterRunner():
         user_input = ''
         char_time = 0
         for number in range(10):
-            self.rando_string += self.all_var[random.randint(0,len(self.all_var) - 1)]
+            self.rando_string += (
+                self.all_var[random.randint(0, len(self.all_var) - 1)])
         while progress != len(self.rando_string) - 1:
             if progress < 0:
-                progress =0
+                progress = 0
             os.system('cls')
             print(f'Input time: {char_time:0.4f}.')
             print(f'The b.a.s. is {self.rando_string}.')
-            print(f'your progress is {self.rando_string[:progress + 1]}' + '\n')
+            print(f'your progress is {self.rando_string[:progress + 1]}' +
+                  '\n')
             start_time = time.perf_counter()
             user_input = input('Enter your character: ')
             end_time = time.perf_counter()
@@ -86,15 +88,15 @@ class CharacterRunner():
                 break
             else:
                 progress -= 1
-                
-                
+
     def mainscreen(self):
         '''
         This lets the user choose which type of character set they want to
         practice.
         '''
         print('What do you want to train?')
-        print('1.)Numbers\n2.)Letters\n3.)Symbols\n4.)Keypad\n5.)For words.\n6.)Build-a-String')
+        print(('1.)Numbers\n2.)Letters\n3.)Symbols\n4.)Keypad\n5.)For words.\
+               \n6.)Build-a-String'))
         game_type = input('Enter game choice ')
         if game_type == '1':
             self.run_game(self.numbers_var)
@@ -108,8 +110,8 @@ class CharacterRunner():
             self.run_game(self.word_list)
         elif game_type == '6':
             self.build_a_string()
-        
-    
+
+
 def start_game():
     start = CharacterRunner()
     start.mainscreen()
